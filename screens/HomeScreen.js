cda4611424d92f986370933bf8524967de39b269
afmire877 <33981371+afmire877@users.noreply.github.com>
2019-11-09 16:17:10 +0000

    Starter commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
